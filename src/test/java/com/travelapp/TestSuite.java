4566c242e56b7d1240f65f214edb6904722b3d64
jbostik <ebiven1563@gmail.com>
2020-06-05 11:32:09 -0400

    Added a TestSuite to run all tests, and added tests to UserServiceTest to get 100% test coverage

--- src/test/java/com/travelapp/TestSuite.java
+++ src/test/java/com/travelapp/TestSuite.java
@@ -1,4 +1,14 @@
 package com.travelapp;
 
-public class TestSuite {
-}
+import com.travelapp.services.TicketServiceTest;
+import com.travelapp.services.UserServiceTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        TicketServiceTest.class,
+        UserServiceTest.class
+})
+
+public class TestSuite {}
